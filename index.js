ec69863388973cce08111f72d2102b536514e2bc
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-07 09:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Code your solution in this file!
-const 
-
-returnFirstTwoDrivers= function(arr){
+const returnFirstTwoDrivers= function(arr){
 let arif=[];
 
 arif.push(arr[0],arr[1]);
 
 console.log(arif);
-}
+};
